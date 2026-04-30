c8ca3ca6d061656d2adb8464f9ba1be49f1edef2
CodeArcadia-fastest <leonardoborbosa06@gmail.com>
2026-04-30 16:58:32 +0500

    First

---
+++ backend/shop/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+
+from shop.models import Category, Product
+from shop.serializers import CategorySerializer, ProductSerializer
+
+# Create your views here.
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
